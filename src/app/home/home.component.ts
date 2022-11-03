280a5ec5cf4ee658495a96182158db10e24c5183
Andre Serfontein <andre.serfontein5@gmail.com>
2022-11-03 21:05:17 +0200

    The Daniel Refactor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,63 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  pseudoMultiplier: number = 100;
   randomNumber!: number;
   procMessage!: string;
   allNumbers: number[] = [];
   allProcs: number[] = [];
+  baseProcChance: number = 50;
   procChance!: number;
-  failProcs: 0 = 0;
   successProcs: 0 = 0;
   averageProcs!: number;
-  totalNumbersGenerated: number = 0;
-  test!: number;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.procChance = 50;
+    this.procChance = this.baseProcChance;
   }
 
-  genNumb() {
-    this.test = Math.random();
-    console.log(this.test);
+  execute() {
+    this.genNum();
+    this.checkProc();
+  }
+
+  genNum() {
     this.randomNumber = Math.floor(Math.random() * 100);
     this.allNumbers.push(this.randomNumber);
-    this.checkProc();
-    this.averageProcs = Math.floor(this.successProcs / this.allProcs.length * 100);
-    this.totalNumbersGenerated = this.allProcs.length;
   }
 
   checkProc() {
-    if (this.randomNumber <= this.procChance)
-      this.allProcs.push(1)
-    // this.failProcs++; Do I even need this??
-    else {
+    if (this.randomNumber < this.baseProcChance) {
+      this.allProcs.push(1);
+    } else {
       this.allProcs.push(0);
       this.successProcs++;
     }
+    this.averageProcs = Math.floor(this.successProcs / this.allProcs.length * 100);
+    this.procChance = this.baseProcChance + (this.baseProcChance - this.averageProcs) * this.pseudoMultiplier;
   }
 
-  // TODO: Add way to let user choose any(?) number of random numbers
-  genTenNumb() {
-    var times = 10;
-    for (var i = 0; i < times; i++) {
-      this.genNumb();
-    }
-  }
-
-  genHundredNumb() {
-    var times = 100;
-    for (var i = 0; i < times; i++) {
-      this.genNumb();
-    }
-  }
-
-  genThousandNumb() {
-    var times = 1000;
-    for (var i = 0; i < times; i++) {
-      this.genNumb();
+  executeTimes(times: number) {
+    for (let i = 0; i < times; i++) {
+      this.execute();
     }
   }
 
