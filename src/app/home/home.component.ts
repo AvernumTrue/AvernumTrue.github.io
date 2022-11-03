677f34a213dd784dadce71ac9c47523933d0c0a2
Andre Serfontein <andre.serfontein5@gmail.com>
2022-11-03 15:14:34 +0200

    Create proc tester on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  randomNumber!: number;
+  procMessage!: string;
+  allNumbers: number[] = [];
+  allProcs: number[] = [];
+  procChance!: number;
+  failProcs: 0 = 0;
+  successProcs: 0 = 0;
+  averageProcs!: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.procChance = 50;
+    // procChance is the minimum number as a percenatge to proc
+  }
+
+  genNumb() {
+    this.randomNumber = Math.floor(Math.random() * 100);
+    this.allNumbers.push(this.randomNumber);
+    this.checkProc();
+    this.averageProcs = Math.floor(this.successProcs / this.allProcs.length * 100);
+  }
+
+  checkProc() {
+    if (this.randomNumber >= this.procChance)
+      this.allProcs.push(1),
+        this.failProcs -= 1
+    else {
+      this.allProcs.push(0);
+      this.successProcs++;
+    }
+  }
+
+  genHundredNumb() {
+    var times = 100;
+    for (var i = 0; i < times; i++) {
+      this.genNumb();
+    }
   }
 
 }
